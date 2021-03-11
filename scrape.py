b650468a265089e23995475059d659f90b6ee61f
elnikio <lopotam@gmail.com>
2021-03-12 01:03:08 +0200

    Added some exception handling and experimented with tags.

--- scrape.py
+++ scrape.py
@@ -1,9 +1,29 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
 ##from html.parser import HTMLParser
 from bs4 import BeautifulSoup
-html = urlopen('https://www.imdb.com/title/tt7939766/?ref_=fn_al_tt_1')
-soup = BeautifulSoup(html, 'html.parser')
-##print(soup.prettify())
-print(type(soup))
-print(soup.title)
-print(soup.body.prettify()
+
+# exception handling :
+try:
+    html = urlopen('https://www.imdb.com/title/tt7939766/?ref_=fn_al_tt_1')
+
+except HTTPError as hrr:
+    # hrr is just an alias for HTTPError
+    print(hrr)
+    print("Page not found.")
+
+except URLError as urr:
+    # urr is just an alias for URLError
+    print(urr)
+    print("Server not found.")
+
+else:
+    print("HTTP and and Server found!")
+    
+    soup = BeautifulSoup(html, 'html.parser')
+    ##print(soup.prettify())
+    print(type(soup))
+    print(soup.find('a').find('href'))
+    #print("tag a:",soup.a['href'])
+    ##print(soup.body.prettify())
